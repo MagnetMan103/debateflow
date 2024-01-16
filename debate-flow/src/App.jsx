dcc01a33dd55da6e7ea5f563233357de97282d31
Alan Munschy <alanmunschy@gmail.com>
2024-01-16 16:26:54 -0500

    first commit

---
+++ debate-flow/src/App.jsx
@@ -0,0 +1,20 @@
+import Textbox from "./textbox.jsx";
+
+function App() {
+    return (
+        <>
+            Debate Flow
+            <div id={"container"}>
+                <Textbox/>
+                <Textbox/>
+                <Textbox/>
+                <Textbox/>
+                <Textbox/>
+
+            </div>
+        </>
+    );
+
+}
+
+export default App
---
+++ debate-flow/src/textbox.jsx
@@ -0,0 +1,9 @@
+
+function Textbox(){
+    return (
+        <textarea className={"text"} placeholder={"Type Here"} rows={"50"} cols={"20"}>
+        </textarea>
+    );
+}
+
+export default Textbox
