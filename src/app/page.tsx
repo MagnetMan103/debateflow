78120f045996efbe9b4c24f257235a06ef08527b
Alan Munschy <alanmunschy@gmail.com>
2024-09-30 01:42:07 -0400

    Removed random semicolon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function HomePage() {
                         <FlowPreview title={item}/>
                     </div>
                 );
-          })};
+          })}
           </div>
           </div>
 
